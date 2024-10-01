2b2fd4a4bcb7a4a6c7c11a17e68b904b2caff20d
Seojin.K <7701seojin@gmail.com>
2024-10-02 05:48:56 +0900

    이미지 출력 종료

--- follow_random_arrow.py
+++ follow_random_arrow.py
@@ -1,2 +1,27 @@
 from pico2d import*
 
+open_canvas()
+back_ground = load_image('TUK_GROUND.png')
+boy = load_image('animation_sheet.png')
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+running = True
+
+while running:
+    clear_canvas()
+    back_ground.draw(400, 300, 800, 600)
+    boy.clip_draw(0, 0, 100, 100, 300, 400, 40, 60)
+    update_canvas()
+    handle_events()
+    delay(0.05)
+
+close_canvas()
